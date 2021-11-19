94d6ae453a085a5f990e98fe4561c92236de400f
daczecha <daczecha@gmail.com>
2021-11-19 23:45:20 +0400

    Add player vs player

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
+class Player{
+    constructor(name,score,display){
+        this.score = score;
+        this.display = display;
+        this.name = name;
+    }    
+    
+    win(){
+        this.score++;
+        this.display.innerText = this.score;
+        popModal(this.name);
+    }    
+}    
+
+let PvP = false;
+let PvC = false;
+
+
+
+const $modal = document.getElementById("modal");
+const $blur = document.getElementById("blur");
+
+
+        
+const $playerScore = document.getElementById('player_score');
+const $opponentScore = document.getElementById('opponent_score');
+
+
+const $player = document.getElementById('player_name_display');
+const $opponent = document.getElementById('opponent_name_display');
+
+
+const $playerName = document.getElementById('player_name').value;
+const $opponentName = document.getElementById('opponent_name').value;
+
+let player = new Player($playerName, 0, $playerScore);
+let opponent = new Player($opponentName, 0, $opponentScore);         
+
+if(player.name === ""){
+    player.name = 'PLAYER ONE';
+}
+
+if(opponent.name === ""){
+    opponent.name = "PLAYER TWO"; 
+}
+
+$player.innerText = player.name;
+$opponent.innerText = opponent.name;
+
 let board = new Array(9);
 
 let xTurn = true;
+
+setActive();    
+
+
 
 
 function play(index){
@@ -15,12 +68,8 @@ function play(index){
             document.getElementById(`${index}_sign`).classList.add('far', 'fa-circle');
             xTurn = !xTurn;
         }
+        setActive();
         checkWin();
-    }
-}
-
-function updateBoard(){
-    for(let i = 0; i < board.length; i++){
     }
 }
 
@@ -37,7 +86,7 @@ function checkWin(){
         (board[0]+board[4]+board[8]) === 3 ||
         (board[6]+board[4]+board[2]) === 3
     ){
-        alert('X wins!');
+        player.win();
     }else if (
         (board[0]+board[1]+board[2]) === 0 ||
         (board[3]+board[4]+board[5]) === 0 ||
@@ -51,10 +100,10 @@ function checkWin(){
         (board[6]+board[4]+board[2]) === 0
 
     ){
-        alert('O wins!');
+        opponent.win();
     }else{
         if(checkBoardFull()){
-            alert('Draw!');
+            popModal("Draw!");
         }
     }
 }
@@ -69,4 +118,101 @@ function checkBoardFull(){
         }
     }
     return isFull;
+}
+
+
+function restart(){
+    board = new Array(9);
+    for(let i = 0 ; i < 9; i++){
+        document.getElementById(`${i}_sign`).classList.remove('fas','far','fa-circle','fa-times');
+    }
+    player.score = 0;
+    opponent.score = 0;
+    $playerScore.innerText = 0;
+    $opponentScore.innerText = 0;
+
+    $blur.style.display = 'none';
+    $modal.style.display = 'none';
+
+    document.getElementById("game").style.display = 'none';
+    document.getElementById("game-settings").style.display = 'flex';
+
+    PvP = false;
+    PvC = false;
+}
+
+function replay(){
+    board = new Array(9);
+    for(let i = 0 ; i < 9; i++){
+        document.getElementById(`${i}_sign`).classList.remove('fas','far','fa-circle','fa-times');
+    }
+
+    $blur.style.display = 'none';
+    $modal.style.display = 'none';
+}
+
+function setActive(){
+    if(xTurn){
+        document.getElementById('player').classList.add('active');
+        document.getElementById('opponent').classList.remove('active');
+    }else{
+        document.getElementById('player').classList.remove('active');
+        document.getElementById('opponent').classList.add('active');
+    }
+}
+
+function popModal(str){
+    $blur.style.display = 'block';
+    $modal.style.display = 'flex';
+
+    if(str !== "Draw!")
+        document.getElementById('message').innerText = `${str} WINS!`;
+    else
+        document.getElementById('message').innerText = `${str}`;
+    document.getElementById('player_score_modal').innerText = player.score;
+    document.getElementById('opponent_score_modal').innerText = opponent.score;
+}
+
+
+function selectMode(mode){
+    if(mode === "player-vs-player"){
+        PvP = true;
+        PvC = false;
+
+        document.getElementById('player-vs-player').classList.add('selected');
+        document.getElementById('player-vs-computer').classList.remove('selected');
+
+        document.getElementById("player_name").disabled = false; 
+        document.getElementById("opponent_name").disabled = false; 
+
+        document.getElementById("pvc_player_name").disabled = true; 
+        document.getElementById("easy").disabled = true; 
+        document.getElementById("hard").disabled = true; 
+        document.getElementById("impossible").disabled = true; 
+
+    }else if(mode === "player-vs-computer"){
+        PvC = true;
+        PvP = false;
+
+        document.getElementById('player-vs-computer').classList.add('selected');
+        document.getElementById('player-vs-player').classList.remove('selected');
+
+        document.getElementById("pvc_player_name").disabled = false; 
+        document.getElementById("easy").disabled = false; 
+        document.getElementById("hard").disabled = false; 
+        document.getElementById("impossible").disabled = false; 
+
+        document.getElementById("player_name").disabled = true; 
+        document.getElementById("opponent_name").disabled = true; 
+    }else{
+        
+    }
+}
+
+
+function startGame(){
+    if(PvP === true){
+        document.getElementById("game").style.display = 'flex';
+        document.getElementById("game-settings").style.display = 'none';   
+    }
 }
